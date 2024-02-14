106b5290087472a27471f23d61153ad856f6723c
Yusuf Gözüm <139639043+gozumyusuff@users.noreply.github.com>
2024-02-14 21:35:13 +0300

    Setting the obstacle generator

---
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    private const float V = -3f;
+    public GameObject[] ObstaclePrefabs;
+    public Transform[] ObstaclePositionZ;
+    public Transform[] ObstaclePositionX;
+
+
+    float yPosition = V;
+
+    
+
+    void Start()
+    {
+        GenerateObstacles();
+    }
+
+    private void GenerateObstacles()
+    {
+        for (int i = 0; i < ObstaclePositionZ.Length; i++)
+        {
+            //rastgele engel seçme
+            GameObject selectedObstaclePrefab = ObstaclePrefabs[Random.Range(0, ObstaclePrefabs.Length)];
+
+            //seçilen prefabi instantiate etme
+            GameObject instantiatedObstacle = Instantiate(selectedObstaclePrefab, this.transform);
+
+            //engelin konumu
+            instantiatedObstacle.transform.localPosition = new Vector3(
+                        ObstaclePositionX[Random.Range(0, ObstaclePositionX.Length)].localPosition.x,
+                        yPosition,
+                        ObstaclePositionZ[i].localPosition.z);
+        }                
+    }
+}
